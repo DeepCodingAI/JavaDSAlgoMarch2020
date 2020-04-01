d7e6e1571f9bbebaced667021071acc099869147
mrahman <rahmanww@gmail.com>
2020-04-01 19:48:28 -0400

    format

--- src/sorting/UseInsertionSort.java
+++ src/sorting/UseInsertionSort.java
@@ -2,7 +2,8 @@ package sorting;
 
 public class UseInsertionSort {
     public static void main(String[] args) {
-        System.out.println("Hello Develop");int [] array = {12,9,1,8,4,7,6,3};
+        System.out.println("Hello Develop");
+        int [] array = {12,9,1,8,4,7,6,3};
 
     }
 }
